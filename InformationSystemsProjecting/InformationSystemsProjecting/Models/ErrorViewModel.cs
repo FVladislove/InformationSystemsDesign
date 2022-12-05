73308f7e63f890ff2a4959891286375c76e9281a
FVladislove <f.vlad.is.love@gmail.com>
2022-12-05 19:40:49 +0200

    initial commit

---
+++ InformationSystemsProjecting/InformationSystemsProjecting/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InformationSystemsProjecting.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
