c9eac8a896731881320ad1e142f4b53dd0854e6c
FVladislove <f.vlad.is.love@gmail.com>
2022-12-08 16:26:53 +0200

    added scaffolder table model StrRozv

---
+++ InformationSystemsDesign/InformationSystemsDesign/Models/StrRozv.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InformationSystemsDesign.Models;
+
+public partial class StrRozv
+{
+    public string CdVyr { get; set; } = null!;
+
+    public string CdSb { get; set; } = null!;
+
+    public string CdKp { get; set; } = null!;
+
+    public int QtyKp { get; set; }
+
+    public int RivNb { get; set; }
+
+    public string RivGrf { get; set; } = null!;
+}
