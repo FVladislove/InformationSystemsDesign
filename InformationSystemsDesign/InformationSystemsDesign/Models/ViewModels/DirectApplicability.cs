8cc180695c22a71391b933da04819a92e6de693d
FVladislove <f.vlad.is.love@gmail.com>
2022-12-11 18:33:54 +0200

    added direct, overall and structural applicability functions and views for Spec

---
+++ InformationSystemsDesign/InformationSystemsDesign/Models/ViewModels/DirectApplicability.cs
@@ -0,0 +1,12 @@
+﻿namespace InformationSystemsDesign.Models.ViewModels
+{
+    public class DirectApplicability
+    {
+        public string ComponentCode { get; set; } = null!;
+
+        public string ComponentName { get; set; } = null!;
+
+        public int Quanity { get; set; } = default!;
+
+    }
+}
---
+++ InformationSystemsDesign/InformationSystemsDesign/Models/ViewModels/StructuralApplicability.cs
@@ -0,0 +1,13 @@
+﻿namespace InformationSystemsDesign.Models.ViewModels
+{
+    public class StructuralApplicability
+    {
+        public string ComponentCode { get; set; } = null!;
+
+        public string ComponentName { get; set; } = null!;
+
+        public int Quanity { get; set; } = default!;
+
+        public string EntryLevel { get; set; } = null!;
+    }
+}
